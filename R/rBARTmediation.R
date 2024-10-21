7d8afc7bfa828b26de56600c844460d0b8ee05d0
WoojungBae <matt.woojung@gmail.com>
2024-10-20 23:48:48 -0400

    update+

--- R/rBARTmediation.R
+++ R/rBARTmediation.R
@@ -134,16 +134,18 @@ rBARTmediation = function(Y, M, C, V, Uindex=NULL,
     if(is.na(Mlambda)) {
       if(is.na(Msigest)) {
         if(pm < n) {
-          dataM = data.frame(t(matX),u0.index,M)
-          if (qr(matX)$rank == pm){
-            namesM = names(dataM)[1:pm]
-            formulM <- stats::as.formula(paste0("M~",paste(namesM, collapse="+")))
-            lmeMtemp <- lme(formulM, random = ~ 1 | factor(u0.index), dataM)
-          } else {
-            namesM = names(dataM)[1:ncol(C)]
-            formulM <- stats::as.formula(paste0("M~",paste(namesM, collapse="+")))
-            lmeMtemp <- lme(M~.,random=~1|factor(u0.index),data.frame(t(matX),u0.index,M))
-          }
+          lmeMtemp <- lme(M~.,random=~1|factor(u0.index),data.frame(t(matX),u0.index,M))
+          # dataM = data.frame(t(matX),u0.index,M)
+          # # dataM = data.frame(apply(matX, 1, scale),u0.index,M)
+          # if (qr(matX)$rank == pm){
+          #   namesM = names(dataM)[1:pm]
+          #   formulM <- stats::as.formula(paste0("M~",paste(namesM, collapse="+")))
+          #   lmeMtemp <- lme(formulM, random = ~ 1 | factor(u0.index), dataM)
+          # } else {
+          #   namesM = names(dataM)[1:(ncol(C)+1)]
+          #   formulM <- stats::as.formula(paste0("M~",paste(namesM, collapse="+")))
+          #   lmeMtemp <- lme(formulM, random = ~ 1 | factor(u0.index), dataM)
+          # }
           Msigest <- as.numeric(VarCorr(lmeMtemp)[2,2])
           uM <- c(lmeMtemp$coefficients$random[[1]])
           if(length(B_uM)==0) {
@@ -181,16 +183,18 @@ rBARTmediation = function(Y, M, C, V, Uindex=NULL,
     if(is.na(Ylambda)) {
       if(is.na(Ysigest)) {
         if(py < n) {
-          dataY = data.frame(t(matM),u0.index,Y)
-          if (qr(matM)$rank == py){
-            namesY = names(dataY)[1:py]
-            formulY <- stats::as.formula(paste0("Y~",paste(namesY, collapse="+")))
-            lmeYtemp <- lme(formulY, random = ~ 1 | factor(u0.index), dataY)
-          } else {
-            namesY = names(dataY)[1:ncol(C)]
-            formulY <- stats::as.formula(paste0("Y~",paste(namesY, collapse="+")))
-            lmeYtemp <- lme(formulY, random = ~ 1 | factor(u0.index), dataY)
-          }
+          lmeYtemp <- lme(Y~.,random=~1|factor(u0.index),data=data.frame(t(matM),u0.index,Y))
+          # dataY = data.frame(t(matM),u0.index,Y)
+          # # dataY = data.frame(apply(matM, 2, scale), u0.index, Y)
+          # if (qr(matM)$rank == py){
+          #   namesY = names(dataY)[1:py]
+          #   formulY <- stats::as.formula(paste0("Y~",paste(namesY, collapse="+")))
+          #   lmeYtemp <- lme(formulY, random = ~ 1 | factor(u0.index), dataY)
+          # } else {
+          #   namesY = names(dataY)[1:(ncol(C)+1)]
+          #   formulY <- stats::as.formula(paste0("Y~",paste(namesY, collapse="+")))
+          #   lmeYtemp <- lme(formulY, random = ~ 1 | factor(u0.index), dataY)
+          # }
           Ysigest <- as.numeric(VarCorr(lmeYtemp)[2,2])
           uY <- c(lmeYtemp$coefficients$random[[1]])
           if(length(B_uY)==0) {
